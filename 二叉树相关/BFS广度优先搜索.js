e5adf962f1267738d3305b0f0c50e3ea0625b680
施逸非 <oreo0226@163.com>
2021-05-19 16:23:56 +0800

    20210519

--- 二叉树相关/BFS广度优先搜索.js
+++ 二叉树相关/BFS广度优先搜索.js
@@ -1,13 +1,16 @@
 var levelOrder = function(root) {
-  if(root==null)return [];
-  //bfs遍历即可
-  let res=[];
-  let qu=[root];
-  while(qu.length>0){
-      let p=qu.shift();
-      res.push(p.val);
-      if(p.left!==null)qu.push(p.left);
-      if(p.right!==null)qu.push(p.right);
-  }
-  return res;
+  var ans = [] //定义一个数组
+  const res = (root, k) => {
+    if (!root) return //如果root为空节点，返回
+    if (k === ans.length) { //k=0时，增加一个数组到ans中，此时ans变成二维数组
+        ans.push([])
+    }
+    ans[k].push(root.val)//将值push到子数组元素中
+    res(root.left, k + 1) 
+    //递归root的左右孩子节点，
+    //如果左右孩子节点有值，将值存到ans的数组元素下标为k+1的数组中
+    res(root.right, k + 1)
+    }
+    res(root, 0) //执行代码
+    return ans
 };

91d75cf5faaad09ae15b16cbbfb4165c2d1d1e21
施逸非 <oreo0226@163.com>
2021-05-20 16:34:41 +0800

    20210520 night

---
+++ 二叉树相关/对称二叉树.js
@@ -0,0 +1,10 @@
+var isS = function(root) {
+  if(!root) return true;
+  const check = (left, right) => {
+    if (!left && !right) return true // 左右子树为空，为镜像
+    if (!left || !right) return false // 左右子树一个为空，不为镜像
+    if (left.val !== right.val) return false // 左右子树值不相等，不为镜像
+    return check(left.left, right.right) && check(left.right, right.left)
+  }
+    return check(root.left, root.right)
+}

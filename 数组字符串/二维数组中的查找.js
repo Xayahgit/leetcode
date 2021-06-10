55c3e2942900d60f671758d24da0b2d540df2442
施逸非 <oreo0226@163.com>
2021-06-10 21:58:37 +0800

    20210610n

---
+++ 数组字符串/二维数组中的查找.js
@@ -0,0 +1,3 @@
+var findNum = function(matrix,target) {
+  return matrix.flat(2).includes(target)
+}
--- 数组字符串/构建乘积数组.js
+++ 数组字符串/构建乘积数组.js
@@ -1,14 +1,18 @@
 var constructArr = function(a) {
   if(!Array.isArray(a) || !a.length) return []
-  let res = []
-  res[0] = 1;
+  let left = []
+  let right = []
+  left[0] = 1;
+  right[a.length-1] = 1; 
   for(let i=1;i<a.length;i++) {
-    res[i] = res[i-1] * a[i-1]
+    left[i] = left[i-1] * a[i-1]
   }
-  let temp = 1;
   for(let i=a.length-2;i>=0;i--) {
-    temp = temp * a[i+1]
-    res[i] = res[i] * temp
+    right[i] = right[i+1] * a[i+1]
+  }
+  let res =[]
+  for(let i=0;i<a.length;i++) {
+    res[i] = left[i] * right[i]
   }
   return res
-}
+};
